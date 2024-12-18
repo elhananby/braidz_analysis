5c9ae8e9359021e0e7f5ea1998d458caae98773b
Elhanan Buchsbaum <elhanan.buchsbaum@mpinb.mpg.de>
2024-12-18 23:04:26 +0100

    fixed __init__.py

--- src/braidz_analysis/__init__.py
+++ src/braidz_analysis/__init__.py
@@ -1,3 +1,9 @@
-# import all modules from folder using "__all__"
+# Import the modules
+from . import processing
+from . import plotting
+from . import helpers
+from . import trajectory
+from . import braidz
 
+# Define what should be included when using "from braidz_analysis import *"
 __all__ = ["processing", "plotting", "helpers", "trajectory", "braidz"]
